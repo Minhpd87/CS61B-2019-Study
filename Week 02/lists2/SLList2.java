de1e77b06d2874f2ef33c5d1120cd82ee5f4d939
Minh Pham Duc <minhpd.87@gmail.com>
2019-04-25 21:46:55 +0700

    Added the addFirst(), getFirst(), addLast(), size() method for the SSList2 class which is a run along side reading 2.2 video.

--- Week 02/lists2/SLList2.java
+++ Week 02/lists2/SLList2.java
@@ -10,6 +10,10 @@ public class SLList2 {
         SLList2 Y = new SLList2(1);
         Y.addFirst(3);
         System.out.println(Y.getFirst());
+        Y.addLast(6);
+        Y.addLast(5);
+        Y.addLast(4);
+        System.out.println(Y.size());
     }
 
     /* add an item to the front of the list */
@@ -23,7 +27,28 @@ public class SLList2 {
         return first.item;
     }
 
-    public static class IntNode {
+    /* Add an item to the end of the list */
+    public void addLast(int x) {
+        //getting to next (IntNode) of the last item in the original list
+        IntNode List = first; //use this to access the IntNode in the SSList, IntNode = first.next
+        while (List.next != null) { //loop stop when List.next = null meaning reached end item
+            List = List.next; //cycle through each IntNode in the SSLIST
+        }
+        List.next = new IntNode(x, null); //add x to the last IntNode
+    }
+
+    /* Return the size of the List */
+    public int size() {
+        int size = 1; //start at 1 because we start increasing size from the next item in the SSList
+        IntNode List = first;
+        while (List.next != null) {
+            size++;
+            List = List.next;
+        }
+        return size;
+    }
+
+    private static class IntNode {
         public int item;
         public IntNode next;
 
