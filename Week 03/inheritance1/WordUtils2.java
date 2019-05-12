71bf2782e98e0747b878ff312f158466e868a00c
Minh Pham Duc <minhpd.87@gmail.com>
2019-05-12 16:31:54 +0700

    WordUtils 2 added

---
+++ Week 03/inheritance1/WordUtils2.java
@@ -0,0 +1,19 @@
+public class WordUtils2 {
+    public static String longest(SLList<String> stringList) {
+        String longest = stringList.get(0);
+        for (int i = 1; i < stringList.size(); i++) {
+            if (longest.length() < stringList.get(i).length()) {
+                longest = stringList.get(i);
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        SLList<String> someList = new SLList<>();
+        someList.addLast("elk");
+        someList.addLast("are");
+        someList.addLast("watching");
+        System.out.println(longest(someList));
+    }
+}
